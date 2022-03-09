aa1ad2eb2cb65efec69ef3d9b12390c47b526bd8
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-03-09 15:50:30 -0500

    Update Binary.cpp

--- Binary.cpp
+++ Binary.cpp
@@ -3,72 +3,85 @@ using namespace std;
 
 string toBinary(int number) {
 	//Variable to store the binary
-	string result = ""; 
+	string result = "";
 
 	do {
 		//Get the remainder of dividing the number by 2
-		int remainder = /*to be completed*/
+		int remainder = /* todo */;
 
-			//If number / 2 has remainder 0
-			if (/*to be completed*/) {
-				result = '0' + result;
-			}
-		//If number / 2 has remainder 1
-			else if (/*to be completed*/d) {
-				result = '1' + result;
-			}
+		//If remainder 0
+		if (/* todo */) {
+			result = '0' + result;
+		}
+		//If remainder 1
+		else if (/* todo */) {
+			result = '1' + result;
+		}
 		//Divide number by 2
-		number = /*to be completed*/;
+		/* todo */
 
-	} while (/*to be completed*/); //Loop while the number is not 0
+	} while (/* todo */); //Loop while the number is not 0
 
 	//Return the string containing the binary
-	return result; 
+	return result;
 }
-
-
-//int toDecimal(string binary) {
-//	int total = 0; //Will store the decimal number
-//
-//	//Set the starting exponent
-//	//For example: 1011 would have a starting exponent of 3
-//
-//	int exponent = /*to be completed*/;    
-//
-//	//Loop through all characters
-//	for (int i = 0; i < binary.length(); i++) { 
-//		
-//		//Check to see if the character is a '1'
-//		if (/*to be completed*/) {       
-//			total = total + pow(2, exponent);
-//		}
-//
-//		//Reduce the exponent by 1
-//		exponent = /*to be completed*/; 
-//	}
-//
-//	//Return the decimal number
-//	return total; 
-//}
-
 
 
 int main()
 {
-
-	//Test toBinary
-	cout << “Enter a number to convert to binary” << endl;
-	int num;
-	cin >> num;
-	cout << toBinary(num) << endl;
-	
-	//Test toDecimal
-	//cout << “Enter a binary number to convert to decimal” << endl;
-	//string binary;
-	//cin >> binary;
-	//cout << toDecimal(binary) << endl;
+	srand(unsigned(time(0)));
+	system("color a");
+	int completed = 0;
 
 
+	while (true) {
+		cout << "-----------------------------------" << endl;
+		cout << "Welcome to the Binary Numbers Game!" << endl;
+		cout << "-----------------------------------" << endl;
+		cout << completed << "/10 complete" << endl << endl;
 
-    system("Pause");
+
+		//Random problem type (0 or 1)
+		int problemType = rand() % 2;
+
+		if (problemType == 0) {
+			/// Binary -> Decimal
+
+			// Generate the decimal number
+			int decimal = rand() % 32;
+			// Convert it to binary
+			string binary = toBinary(decimal);
+
+			//Give user the binary and ask for the decimal
+			cout << "What is " << binary << " in decimal?" << endl;
+			while (true) {
+				int playerAnswer;
+				cin >> playerAnswer;
+
+				//Check player answer
+				if (playerAnswer == decimal) {
+					cout << "Correct!" << endl;
+					completed++;
+					//Leave the loop
+					break;
+				}
+				else {
+					cout << "Try again!" << endl;
+				}
+			}
+		}
+		else {
+			/// Decimal -> Binary
+
+			cout << "This question type has not been completed " << endl;
+		}
+
+		//Pause, then clear the screen before the next round
+		system("pause");
+		system("cls");
+	}
+
+	cout << "Congratulations, You Passed!" << endl << endl;
+
+	system("Pause");
 }
