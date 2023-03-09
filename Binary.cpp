05d26af014ba5bfa89d512855b1c7d4b55623ebd
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2023-03-09 08:05:43 -0500

    Update Binary.cpp

--- Binary.cpp
+++ Binary.cpp
@@ -33,59 +33,22 @@ string getBinary(int number) {
 
 int main()
 {
-	srand(unsigned(time(0)));
-	system("color a");
-	int completed = 0;
+
+	//This code will test your getBinary function.
+	// The ouput should be:
+	// 0
+	// 110
+	// 111
+    	// 10011
+	// 10111
+	cout << getBinary(0) << endl;
+	cout << getBinary(6) << endl;
+	cout << getBinary(7) << endl;
+	cout << getBinary(19) << endl;
+	cout << getBinary(23) << endl;
+	//Once your function works, delete this testing code.
 
 
-	while (true) {
-		cout << "-----------------------------------" << endl;
-		cout << "Welcome to the Binary Numbers Game!" << endl;
-		cout << "-----------------------------------" << endl;
-		cout << completed << "/10 complete" << endl << endl;
 
-
-		//Random problem type (0 or 1)
-		int problemType = rand() % 2;
-
-		if (problemType == 0) {
-			/// Binary -> Decimal
-
-			// Generate the decimal number
-			int decimal = rand() % 32;
-			// Convert it to binary
-			string binary = toBinary(decimal);
-
-			//Give user the binary and ask for the decimal
-			cout << "What is " << binary << " in decimal?" << endl;
-			while (true) {
-				int playerAnswer;
-				cin >> playerAnswer;
-
-				//Check player answer
-				if (playerAnswer == decimal) {
-					cout << "Correct!" << endl;
-					completed++;
-					//Leave the loop
-					break;
-				}
-				else {
-					cout << "Try again!" << endl;
-				}
-			}
-		}
-		else {
-			/// Decimal -> Binary
-
-			cout << "This question type has not been completed " << endl;
-		}
-
-		//Pause, then clear the screen before the next round
-		system("pause");
-		system("cls");
-	}
-
-	cout << "Congratulations, You Passed!" << endl << endl;
-
-	system("Pause");
+    system("pause");
 }
